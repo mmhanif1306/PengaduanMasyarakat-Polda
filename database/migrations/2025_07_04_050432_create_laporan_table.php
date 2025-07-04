f5743ad98134293930afd698ccdcf00f402e4fe0
mhusainh <husainhasbulah@gmail.com>
2025-07-04 12:08:03 +0700

    feat(database): add user and report tables with required fields
    - Add NIK, nama, and role fields to users table
    - Create new laporan table with user relation and status tracking

---
+++ database/migrations/2025_07_04_050432_create_laporan_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('laporan', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->string('judul');
+            $table->text('deskripsi');
+            $table->text('alamat');
+            $table->string('longitude');
+            $table->string('latitude');
+            $table->string('public_id')->nullable();
+            $table->string('url_file')->nullable();
+            $table->enum('status', ['menunggu', 'diproses', 'selesai'])->default('menunggu');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('laporan');
+    }
+};
